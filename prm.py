47fce17ba97250570cb1b94c11409ecb576138f6
Ridhwanluthra <Ridhwancode@gmail.com>
2016-06-28 01:53:00 +0530

    started working on probabilistic roadmap method

--- prm.py
+++ prm.py
@@ -57,44 +57,27 @@ def create_graph(grid):
     print "convert =" ,elapsed_time_convert
     return graph
 
-def dijkstra(Graph, source):
-    '''
-        +   +---+---+
-        | 0   1   2 |
-        +---+   +   +
-        | 3   4 | 5  
-        +---+---+---+
- 
-        >>> graph = (        # or ones on the diagonal
-        ...     (0,1,0,0,0,0,),
-        ...     (1,0,1,0,1,0,),
-        ...     (0,1,0,0,0,1,),
-        ...     (0,0,0,0,1,0,),
-        ...     (0,1,0,1,0,0,),
-        ...     (0,0,1,0,0,0,),
-        ... )
-        ...
-        >>> Dijkstra(graph, 0)
-        ([0, 1, 2, 3, 2, 3], [1e+140, 0, 1, 4, 1, 2])
-        >>> display_solution([1e+140, 0, 1, 4, 1, 2])
-        5<2<1<0
-    '''
-    # Graph[u][v] is the weight from u to v (however 0 means infinity)
+def prm(Graph, source):
     start_time = time.time()
 
     infinity = float('infinity')
     n = len(graph)
-    dist = [infinity]*n   # Unknown distance function from source to v
-    previous = [infinity]*n # Previous node in optimal path from source
-    dist[source] = 0        # Distance from source to source
-    Q = list(range(n)) # All nodes in the graph are unoptimized - thus are in Q
-    while Q:           # The main loop
-        u = min(Q, key = lambda n:dist[n])                 # vertex in Q with smallest dist[]
-        Q.remove(u)
+    #empty list for distances from source
+    dist = [infinity]*n
+    #empty list for the previous node in the path
+    previous = [infinity]*n
+    dist[source] = 0
+    unoptimized_vertices = list(range(n))
+
+    while unoptimized_vertices:
+        # vertex in Q with smallest dist[]
+        u = min(unoptimized_vertices, key = lambda n:dist[n])
+        print u
+        unoptimized_vertices.remove(u)
         if dist[u] == infinity:
             break # all remaining vertices are inaccessible from source
         for v in range(n):               # each neighbor v of u
-            if Graph[u][v] and (v in Q): # where v has not yet been visited
+            if Graph[u][v] and (v in unoptimized_vertices): # where v has not yet been visited
                 alt = dist[u] + Graph[u][v]
                 if alt < dist[v]:       # Relax (u,v,a)
                     dist[v] = alt
@@ -115,8 +98,8 @@ def display_solution(predecessor):
 #grid.print_grid()
 #graph = create_graph(grid.grid)
 #print graph
-dist, previous = dijkstra(graph, 0)
+dist, previous = prm(graph, 0)
 #print len(dist)
 #print len(previous)
-print dist, previous
+#print dist, previous
 display_solution(previous)
--- structure.py
+++ structure.py
@@ -42,6 +42,12 @@ class grid(object):
 		self.add_single_obstacle(5,7)
 		#self.print_grid()
 
+	def big_map(self):
+		self.create_grid(100,100)
+		self.add_obstacle(25,25,0,90)
+		self.add_obstacle(50,50,10,100)
+		self.add_obstacle(75,57,0,90)
+
 	#prints the grid in a more human readable form
 	def print_grid(self):
 		rows = len(self.grid)
